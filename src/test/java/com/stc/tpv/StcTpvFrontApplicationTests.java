cf07dd14325b0a3e9eadce0aa1c5a943b6c61cc8
Job Gonzelez <jgonzalez@bytesw.com>
2022-04-20 20:26:09 -0600

    commit inicial

---
+++ src/test/java/com/stc/tpv/StcTpvFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stc.tpv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcTpvFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
